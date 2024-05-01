efb4abfb0b43a732f56e4997e639afe621bedfb4
lharnist <lharnist@ucsc.edu>
2024-04-30 17:34:38 -0700

    Almost finished linked based game

--- rules.js
+++ rules.js
@@ -1,22 +1,25 @@
 class Start extends Scene {
     create() {
-        this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
+        this.engine.setTitle(this.engine.storyData.Title); // TODO: replace this text using this.engine.storyData to find the story title
         this.engine.addChoice("Begin the story");
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, "Home"); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); // TODO: replace this text by the initial location of the story
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = undefined; // TODO: use `key` to get the data object for the current story location
-        this.engine.show("Body text goes here"); // TODO: replace this text by the Body of the location data
-        
-        if(true) { // TODO: check if the location has any Choices
-            for(let choice of ["example data"]) { // TODO: loop over the location's Choices
-                this.engine.addChoice("action text"); // TODO: use the Text of the choice
+        let locationData = key; // TODO: use `key` to get the data object for the current story location
+        this.engine.show(this.engine.storyData.Locations[key].Body); // TODO: replace this text by the Body of the location data
+        if(this.engine.storyData.Locations[key].preworkout == true){
+            this.engine.storyData.preworkout = true;
+        }
+        if(this.engine.storyData.Locations[key].Choices != {}) { // TODO: check if the location has any Choices
+            for(let choice of this.engine.storyData.Locations[key].Choices){ // TODO: loop over the location's Choices
+                //console.log("This is the current choice: " + choice);
+                this.engine.addChoice(choice.Text, choice); // TODO: use the Text of the choice
                 // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
             }
         } else {
@@ -26,8 +29,29 @@ class Location extends Scene {
 
     handleChoice(choice) {
         if(choice) {
-            this.engine.show("&gt; "+choice.Text);
-            this.engine.gotoScene(Location, choice.Target);
+            //let button happen, check to see if button has precondition, 
+            //if no let button happen, if yes check precondition of "preworkout" and preworkout is true
+            
+            
+            if(choice.Condition){
+                let conditionName = Object.keys(choice.Condition)[0];
+                if(this.engine.storyData[conditionName] == true){
+                    //console.log(Location);
+                    this.engine.gotoScene(Location, choice.Target);
+                    this.engine.show("&gt; "+choice.Text);
+                } else {
+                    
+                    console.log(this.engine.storyData.Locations.NotYet.Choices[0]);
+                    console.log(this.engine.storyData.Locations.NotYet);
+                    
+                    //this.engine.gotoScene(this.engine.storyData.Locations.NotYet, this.engine.storyData.Locations.NotYet.Choices[0]);
+                    //this.engine.show("&gt; "+this.storyData.Locations.NotYet.Text);
+                }
+            } else {
+                this.engine.gotoScene(Location, choice.Target);
+                this.engine.show("&gt; "+choice.Text);
+            }
+
         } else {
             this.engine.gotoScene(End);
         }
